19538f7b591bc97f3e6a8721ebc978d01503f06c
L-Y-V-C <luigi.valenzuela@ucsp.edu.pe>
2026-04-13 13:03:37 +0000

    feat: adding multiple message options, for broadcast, unicast, also log in and out functions for clients, all following a simple protocol based in size and data

---
+++ v1.2/client.cpp
@@ -0,0 +1,442 @@
+ /* Client code in C */
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <iostream>
+#include <thread>
+//#include <atomic>
+
+using namespace std;
+
+//atomic<int> connected(1);
+
+void convertIntToString(int byteSize, int num, char *str);
+void sendFunction(int SocketFD);
+void receiveFunction(int SocketFD);
+void processMessage(int SocketFD);
+void print1stMenu(int &op);
+void print2ndMenu(int &op);
+void checkOption(int op, int menu, int SocketFD);
+void makeMessage(int op, int SocketFD);
+
+void print1stMenu(int &op)
+{
+	char tmp[10];
+	printf(
+		"-------------------------------\n"
+		"|            Menu             |\n"
+		"|Options:                     |\n"
+		"|  [1] - Login                |\n"
+		"|  [2] - Exit                 |\n"
+		"-------------------------------\n"
+	);
+	fflush(stdout);
+	fgets(tmp, sizeof(tmp), stdin);
+	op = atoi(tmp);
+}
+
+void print2ndMenu(int &op)
+{
+	char tmp[10];
+	printf(
+		"-------------------------------\n"
+		"|           Messages          |\n"
+		"|  [1] - Broadcast            |\n"
+		"|  [2] - Unicast              |\n"
+		"|  [3] - Send file            |\n"
+		"|           Others            |\n"
+		"|  [4] - Get client list      |\n"
+		"|  [0] - Logout               |\n"
+		"-------------------------------\n"
+	);
+	fflush(stdout);
+	fgets(tmp, sizeof(tmp), stdin);
+	op = atoi(tmp);
+}
+
+void checkOption(int op, int menu, int SocketFD)
+{
+	switch(menu)
+	{
+		case 1:
+			switch(op)
+			{
+				case 1:
+					makeMessage(0, SocketFD);
+					break;
+				case 2:
+					exit(0);
+					break;
+				default:
+					break;
+			}
+			break;
+		case 2:
+			switch(op)
+			{
+				case 1:
+					makeMessage(1, SocketFD);
+					break;
+				case 2:
+					makeMessage(2, SocketFD);
+					break;
+				case 3:
+					makeMessage(3, SocketFD);
+					break;
+				case 4:
+					makeMessage(4, SocketFD);
+					break;
+				default:
+					break;
+			}
+			break;
+		default:
+			break;
+	}
+}
+
+void makeMessage(int op, int SocketFD)
+{
+	char buffer[256], input[256], inputSizeStr[20];
+	int inputSize, offset;
+	int byteSize[] = {4, 7, 5};
+	int byteSizeAux[] = {0, 0, 7};
+
+	offset = 1;
+	bzero(buffer, 256);
+
+	switch(op)
+	{
+		// Login
+		case 0:
+		{
+			buffer[0] = 'L';
+
+			printf("Nickname: ");
+			fflush(stdout);
+			fgets(input, 256, stdin);
+			//buffer[strcspn(buffer, "\n")] = 0;
+
+			inputSize = strlen(input) - 1;
+			convertIntToString(byteSize[op] , inputSize, inputSizeStr);
+
+			memcpy(buffer + offset, inputSizeStr, byteSize[op]);
+			offset += byteSize[op];
+			memcpy(buffer + offset, input, inputSize);
+
+			//printf("msg:\n[%s]\n", buffer);
+			write(SocketFD, buffer, strlen(buffer));
+
+			break;
+		}
+		// send broadcast msg
+		case 1:
+		{
+			buffer[0] = 'B';
+
+			printf("Message: ");
+			fflush(stdout);
+			fgets(input, 256, stdin);
+
+			inputSize = strlen(input) - 1;
+			convertIntToString(byteSize[op] , inputSize, inputSizeStr);
+
+			memcpy(buffer + offset, inputSizeStr, byteSize[op]);
+			offset += byteSize[op];
+			memcpy(buffer + offset, input, inputSize);
+
+			//printf("msg:\n[%s]\n", buffer);
+			write(SocketFD, buffer, strlen(buffer));
+
+			break;
+		}
+		// send unicast msg
+		case 2:
+		{
+			buffer[0] = 'U';
+
+			printf("Message: ");
+			fflush(stdout);
+			fgets(input, 256, stdin);
+
+			inputSize = strlen(input) - 1;
+			convertIntToString(byteSize[op] , inputSize, inputSizeStr);
+
+			memcpy(buffer + offset, inputSizeStr, byteSize[op]);
+			offset += byteSize[op];
+			memcpy(buffer + offset, input, inputSize);
+			offset += inputSize;
+
+			printf("To: ");
+			fflush(stdout);
+			fgets(input, 256, stdin);
+
+			inputSize = strlen(input) - 1;
+			convertIntToString(byteSizeAux[op] , inputSize, inputSizeStr);
+
+			memcpy(buffer + offset, inputSizeStr, byteSizeAux[op]);
+			offset += byteSizeAux[op];
+			memcpy(buffer + offset, input, inputSize);
+
+			//printf("msg:\n[%s]\n", buffer);
+			write(SocketFD, buffer, strlen(buffer));
+
+			break;
+		}
+		// send file
+		case 3:
+		{
+			break;
+		}
+		// who is in the server?
+		case 4:
+		{
+			break;
+		}
+		// Logout
+		case 5:
+		{
+			buffer[0] = 'O';
+			write(SocketFD, buffer, 1);
+			break;
+		}
+		default:
+			break;
+	}
+}
+
+void sendFunction(int SocketFD)
+{
+	int op;//status; // status - x = logged - 0 = logged out
+	//status = 0;
+	op = 0;
+
+	for(;;)
+	{
+		if(!op)
+		{
+			print1stMenu(op);
+			checkOption(op, 1, SocketFD);
+		}
+		print2ndMenu(op);
+		checkOption(op, 2, SocketFD);
+	}
+}
+
+void receiveFunction(int SocketFD)
+{
+	for(;;)
+	{
+		processMessage(SocketFD);
+	}
+}
+
+void processMessage(int SocketFD)
+{
+	char buffer[256], msg[256], msgType, nickname[256];
+	int n, i, tmpSize;
+	int byteSize[] = {5, 3, 7};
+	int byteSizeAux[] = {0, 7, 5};
+
+	bzero(buffer, 256);
+
+	n = read(SocketFD, buffer, 1);
+
+	if(n == 0)
+	{
+		printf("\n"
+			"----------------------\n"
+			"You were disconnected!\n"
+			"----------------------\n"
+		);
+		exit(0);
+		//connected = 0;
+		//return;
+	}
+
+	msgType = buffer[0];
+
+	switch(msgType)
+	{
+		// correct login msg
+		case 'K':
+		{
+			printf("\n"
+				"----------------------------\n"
+				"Nickname saved successfully!\n"
+				"----------------------------\n"
+			);
+			break;
+		}
+		// receive error msg
+		case 'E':
+		{
+			i = 0;
+			n = read(SocketFD, buffer, byteSize[i]);
+			buffer[n] = '\0';
+			tmpSize = atoi(buffer);
+
+			n = read(SocketFD, buffer, tmpSize);
+			buffer[n] = '\0';
+			strcpy(msg, buffer);
+
+			printf("\n"
+				"--------------------------------------------------------\n"
+				"ERROR!\n"
+				"%s\n"
+				"--------------------------------------------------------\n"
+				, msg
+			);
+			fflush(stdout);
+
+			break;
+		}
+		// receive broadcast msg
+		case 'b':
+		{
+			i = 1;
+			n = read(SocketFD, buffer, byteSize[i]);
+			buffer[n] = '\0';
+			tmpSize = atoi(buffer);
+
+			n = read(SocketFD, buffer, tmpSize);
+			buffer[n] = '\0';
+			strcpy(nickname, buffer);
+
+			n = read(SocketFD, buffer, byteSizeAux[i]);
+			buffer[n] = '\0';
+			tmpSize = atoi(buffer);
+
+			n = read(SocketFD, buffer, tmpSize);
+			buffer[n] = '\0';
+			strcpy(msg, buffer);
+
+			printf("\n"
+				"--------------------------------------------------------\n"
+				"From: %s\n"
+				"Msg: %s\n"
+				"--------------------------------------------------------\n"
+				, nickname, msg
+			);
+			fflush(stdout);
+
+			break;
+		}
+		// receive unicast msg
+		case 'u':
+		{
+			i = 2;
+			n = read(SocketFD, buffer, byteSize[i]);
+			buffer[n] = '\0';
+			tmpSize = atoi(buffer);
+
+			n = read(SocketFD, buffer, tmpSize);
+			buffer[n] = '\0';
+			strcpy(nickname, buffer);
+
+			n = read(SocketFD, buffer, byteSizeAux[i]);
+			buffer[n] = '\0';
+			tmpSize = atoi(buffer);
+
+			n = read(SocketFD, buffer, tmpSize);
+			buffer[n] = '\0';
+			strcpy(msg, buffer);
+
+			printf("\n"
+				"--------------------------------------------------------\n"
+				"From: %s\n"
+				"Msg: %s\n"
+				"--------------------------------------------------------\n"
+				, nickname, msg
+			);
+			fflush(stdout);
+
+			break;
+		}
+		// who is in the server?
+		case 't':
+		{
+			break;
+		}
+		// receive file
+		case 'f':
+		{
+			break;
+		}
+		default:
+			break;
+	}
+}
+
+void convertIntToString(int byteSize, int num, char *str)
+{
+	int power10 = 1;
+
+	for(int i = 1; i < byteSize; i++)
+		power10 *= 10;
+
+	for(int i = 0; i < byteSize; i++)
+	{
+		int digit = num / power10;
+		str[i] = '0' + digit;
+		num %= power10;
+		power10 /= 10;
+	}
+}
+
+int main(void)
+{
+	struct sockaddr_in stSockAddr;
+	int Res;
+	int SocketFD = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+	if (-1 == SocketFD)
+	{
+		perror("cannot create socket");
+		exit(EXIT_FAILURE);
+	}
+
+	memset(&stSockAddr, 0, sizeof(struct sockaddr_in));
+
+	stSockAddr.sin_family = AF_INET;
+	stSockAddr.sin_port = htons(45000);
+	Res = inet_pton(AF_INET, "192.168.100.110", &stSockAddr.sin_addr);
+
+	if (0 > Res)
+	{
+		perror("error: first parameter is not a valid address family");
+		close(SocketFD);
+		exit(EXIT_FAILURE);
+	}
+	else if (0 == Res)
+	{
+		perror("char string (second parameter does not contain valid ipaddress");
+		close(SocketFD);
+		exit(EXIT_FAILURE);
+	}
+
+	if (-1 == connect(SocketFD, (const struct sockaddr *)&stSockAddr, sizeof(struct sockaddr_in)))
+	{
+		perror("connect failed");
+		close(SocketFD);
+		exit(EXIT_FAILURE);
+	}
+
+	thread send = thread(sendFunction, SocketFD);
+	thread receive = thread(receiveFunction, SocketFD);
+
+	send.join();
+	receive.join();
+
+	shutdown(SocketFD, SHUT_RDWR);
+	close(SocketFD);
+
+	return 0;
+}
---
+++ v1.2/server.cpp
@@ -0,0 +1,380 @@
+/* Server code in C */
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <iostream>
+#include <thread>
+#include <map>
+#include <string>
+
+#include "json.hpp"
+
+using json = nlohmann::json;
+using namespace std;
+
+map<string, int> clients;
+
+void processMessage(int ConnectFD);
+void sendFunction(int ConnectFD);
+void receiveFunction(int ConnectFD);
+void prepareMessage(int op, char *buffer);
+void convertIntToString(int byteSize, int num, char *str);
+
+void sendFunction(int ConnectFD)
+{
+	char buffer[256];
+	for(;;)
+	{
+		bzero(buffer, 256);
+		fgets(buffer, 256, stdin);
+		write(ConnectFD, buffer, strlen(buffer));
+	}
+}
+
+void receiveFunction(int ConnectFD)
+{
+	for(;;)
+	{
+		processMessage(ConnectFD);
+
+		/*
+		n = read(ConnectFD, buffer, 255);
+
+		if(n <= 0)
+		{
+			close(ConnectFD);
+			break;
+		}
+		*/
+	}
+}
+
+void prepareMessage(int op, char *buffer, char (*data)[256])
+{
+	char msg[256], msgSizeStr[20], tmpSizeStr[20];
+	int msgSize, tmpSize, offset;
+	int byteSize[] = {0, 5, 3, 7};
+	int byteSizeAux[] = {0, 0, 7, 5};
+
+	offset = 1;
+	bzero(msg, 256);
+
+	switch(op)
+	{
+		// notify correct login
+		case 0:
+		{
+			buffer[0] = 'K';
+			buffer[1] = '\0';
+			break;
+		}
+		// notify error
+		case 1:
+		{
+			buffer[0] = 'E';
+
+			strcpy(msg, "ERROR FROM SERVER!");
+			msgSize = strlen(msg);
+			convertIntToString(byteSize[op], msgSize, msgSizeStr);
+
+			memcpy(buffer + offset, msgSizeStr, byteSize[op]);
+			offset += byteSize[op];
+			memcpy(buffer + offset, msg, msgSize);
+
+			break;
+		}
+		// send broadcast
+		case 2:
+		{
+			buffer[0] = 'b';
+
+			tmpSize = strlen(data[0]);
+			convertIntToString(byteSize[op], tmpSize, tmpSizeStr);
+
+			memcpy(buffer + offset, tmpSizeStr, byteSize[op]);
+			offset += byteSize[op];
+			memcpy(buffer + offset, data[0], tmpSize);
+			offset += tmpSize;
+
+			tmpSize = strlen(data[1]);
+			convertIntToString(byteSizeAux[op], tmpSize, tmpSizeStr);
+
+			memcpy(buffer + offset, tmpSizeStr, byteSizeAux[op]);
+			offset += byteSizeAux[op];
+			memcpy(buffer + offset, data[1], tmpSize);
+
+			break;
+		}
+		// send unicast
+		case 3:
+		{
+			buffer[0] = 'u';
+
+			tmpSize = strlen(data[0]);
+			convertIntToString(byteSize[op], tmpSize, tmpSizeStr);
+
+			memcpy(buffer + offset, tmpSizeStr, byteSize[op]);
+			offset += byteSize[op];
+			memcpy(buffer + offset, data[0], tmpSize);
+			offset += tmpSize;
+
+			tmpSize = strlen(data[1]);
+			convertIntToString(byteSizeAux[op], tmpSize, tmpSizeStr);
+
+			memcpy(buffer + offset, tmpSizeStr, byteSizeAux[op]);
+			offset += byteSizeAux[op];
+			memcpy(buffer + offset, data[1], tmpSize);
+
+			break;
+		}
+		// send client list
+		case 4:
+		{
+			break;
+		}
+		default:
+			break;
+	}
+}
+
+void convertIntToString(int byteSize, int num, char *str)
+{
+	int power10 = 1;
+
+	for(int i = 1; i < byteSize; i++)
+		power10 *= 10;
+
+	for(int i = 0; i < byteSize; i++)
+	{
+		int digit = num / power10;
+		str[i] = '0' + digit;
+		num %= power10;
+		power10 /= 10;
+	}
+}
+
+void processMessage(int ConnectFD)
+{
+	char buffer[256], data[3][256], msgType;
+	int n, op, tmpSize;
+	int byteSize[] = {4, 7, 5};
+	int byteSizeAux[] = {0, 0, 7};
+
+	bzero(buffer, 256);
+
+	n = read(ConnectFD, buffer, 1);
+	//buffer[n] = '\0';
+
+	msgType = buffer[0];
+
+	switch(msgType)
+	{
+		// request Login
+		case 'L':
+		{
+			op = 0;
+			n = read(ConnectFD, buffer, byteSize[op]);
+			buffer[n] = '\0';
+			tmpSize = atoi(buffer);
+
+			n = read(ConnectFD, buffer, tmpSize);
+			buffer[n] = '\0';
+			strcpy(data[0], buffer);
+
+			printf("\nuser: %s\n", data[0]);
+
+			if(clients.find(data[0]) != clients.end())
+			{
+				prepareMessage(1, buffer, data);
+			}
+			else
+			{
+				clients[data[0]] = ConnectFD;
+				prepareMessage(0, buffer, data);
+			}
+			write(ConnectFD, buffer, strlen(buffer));
+			//printf("\nmsg: %s\n", buffer);
+
+			break;
+		}
+		// request Logout
+		case 'O':
+		{
+			for(auto it = clients.begin(); it != clients.end(); it++)
+			{
+				if(it->second == ConnectFD)
+				{
+					clients.erase(it);
+					break;
+				}
+			}
+			shutdown(ConnectFD, SHUT_RDWR);
+			close(ConnectFD);
+
+			break;
+		}
+		// request broadcast msg
+		case 'B':
+		{
+			int found;
+			found = 0;
+
+			op = 1;
+
+			n = read(ConnectFD, buffer, byteSize[op]);
+			buffer[n] = '\0';
+			tmpSize = atoi(buffer);
+
+			n = read(ConnectFD, buffer, tmpSize);
+			buffer[n] = '\0';
+			strcpy(data[1], buffer);
+
+			for(auto c : clients)
+			{
+				if(c.second == ConnectFD)
+				{
+					found = 1;
+					strcpy(data[0], c.first.c_str());
+					prepareMessage(2, buffer, data);
+				}
+			}
+
+			if(!found)
+			{
+				prepareMessage(1, buffer, data);
+				write(ConnectFD, buffer, strlen(buffer));
+				break;
+			}
+
+			//printf("\nmsg: %s\n", buffer);
+			for(auto c : clients)
+			{
+				write(c.second, buffer, strlen(buffer));
+			}
+
+			break;
+		}
+		// request unicast msg
+		case 'U':
+		{
+			op = 2;
+
+			n = read(ConnectFD, buffer, byteSize[op]);
+			buffer[n] = '\0';
+			tmpSize = atoi(buffer);
+
+			n = read(ConnectFD, buffer, tmpSize);
+			buffer[n] = '\0';
+			strcpy(data[1], buffer);
+
+			n = read(ConnectFD, buffer, byteSizeAux[op]);
+			buffer[n] = '\0';
+			tmpSize = atoi(buffer);
+
+			n = read(ConnectFD, buffer, tmpSize);
+			buffer[n] = '\0';
+			strcpy(data[2], buffer);
+
+			for(auto c : clients)
+			{
+				if(c.second == ConnectFD)
+				{
+					strcpy(data[0], c.first.c_str());
+				}
+			}
+
+			string nickname = data[2];
+
+			if(auto search = clients.find(nickname); search != clients.end())
+			{
+				prepareMessage(3, buffer, data);
+				//printf("\nmsg: %s\n", buffer);
+				write(search->second, buffer, strlen(buffer));
+			}
+			else
+			{
+				// TODO: ask the professor
+				printf("\nERROR: user not found\n");
+			}
+
+			break;
+		}
+		// who is in the server?
+		case 'T':
+		{
+			json clientListJson;
+
+			for(auto c : clients)
+			{
+				clientList["users"].push_back(c.first);
+			}
+
+			string clientListStr = clientListJson.dump();
+
+			break;
+		}
+		// request file transfer
+		case 'F':
+		{
+			break;
+		}
+		default:
+			break;
+	}
+
+	//buffer[strcspn(buffer, "\n")] = 0;
+}
+
+int main(void)
+{
+	struct sockaddr_in stSockAddr;
+	int SocketFD = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+	if(-1 == SocketFD)
+	{
+		perror("can not create socket");
+		exit(EXIT_FAILURE);
+	}
+
+	memset(&stSockAddr, 0, sizeof(struct sockaddr_in));
+
+	stSockAddr.sin_family = AF_INET;
+	stSockAddr.sin_port = htons(45000);
+	stSockAddr.sin_addr.s_addr = INADDR_ANY;
+
+	if(-1 == bind(SocketFD,(const struct sockaddr *)&stSockAddr, sizeof(struct sockaddr_in)))
+	{
+		perror("error bind failed");
+		close(SocketFD);
+		exit(EXIT_FAILURE);
+	}
+
+	if(-1 == listen(SocketFD, 10))
+	{
+		perror("error listen failed");
+		close(SocketFD);
+		exit(EXIT_FAILURE);
+	}
+
+	for(;;)
+	{
+		int ConnectFD = accept(SocketFD, NULL, NULL);
+
+		//thread(sendFunction, ConnectFD).detach();
+		thread(receiveFunction, ConnectFD).detach();
+
+		//shutdown(ConnectFD, SHUT_RDWR);
+		//close(ConnectFD);
+	}
+
+	close(SocketFD);
+
+	return 0;
+}
